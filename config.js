527f8f0c826363628b8bdaecafc1c3109a1dc850
ice64884 <166520144+ice64884@users.noreply.github.com>
2024-04-09 15:43:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['263715456301', 'ice🧊', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
